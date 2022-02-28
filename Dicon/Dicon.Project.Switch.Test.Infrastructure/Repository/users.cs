de7b3d4f75cf5378831ff6f62cdcd87303d653e7
pal2097 <pal2097@gmail.com>
2022-02-28 23:03:53 +0800

    TEMP

---
+++ Dicon/Dicon.Project.Switch.Test.Infrastructure/Repository/users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dicon.Project.Switch.Test.Infrastructure.Repository
+{
+    public class users
+    {
+        //[Key]
+        public int id { get; set; }
+        public string name { get; set; }
+        public string gender { get; set; }
+        public int age { get; set; }
+        public string hobby { get; set; }
+
+    }
+}
--- Dicon/Dicon.Project.Switch.Testing.UnitTest/DBUnitOfWorkTest.cs
+++ Dicon/Dicon.Project.Switch.Testing.UnitTest/DBUnitOfWorkTest.cs
@@ -1,4 +1,6 @@
-﻿using Dicon.Project.Switch.Test.Infrastructure.Connection;
+﻿using Dicon.Project.Switch.Test.Infrastructure.BaseUnitOfWork;
+using Dicon.Project.Switch.Test.Infrastructure.Connection;
+using Dicon.Project.Switch.Test.Infrastructure.Repository;
 using NUnit.Framework;
 using System.Data;
 
@@ -7,17 +9,42 @@ namespace Dicon.Project.Switch.Testing.UnitTest
 
     public class DBUnitOfWorkTest
     {
+        private DapperUnitOfWork Uow;
         private IDbConnection dbConnection;
+
+        private string LocalConnStr(string dbName)
+        {
+            return $"Data Source=LAPTOP-J0A685IR\\MSSQLSERVER01;Initial Catalog={dbName};Persist Security Info=True;User ID=sa;Password=laser99";
+        }
 
         [SetUp]
         public void Setup()
         {
-            var connStr = "Data Source=GFI_E0373\\SQLEXPRESS;Initial Catalog=1XN_MEMS_SWITCH_DB;Persist Security Info=True;User ID=sa;Password=laser99";
-            var sqlFactory = new SqlDbFactory(connStr);
+            //var connStr = "Data Source=GFI_E0373\\SQLEXPRESS;Initial Catalog=1XN_MEMS_SWITCH_DB;Persist Security Info=True;User ID=sa;Password=laser99";
+            var sqlFactory = new SqlDbFactory(LocalConnStr("Dummy"));
             dbConnection = sqlFactory.Create();
+            Uow = new DapperUnitOfWork(dbConnection);
         }
 
+        [Test]
+        public void When_CompTestingStageRepo_create_dat_it_can_read_value()
+        {
+            var userRepo = Uow.GetGenericRepository<users>();
 
+            var user = new users()
+            {
+                id = 12,
+                name = "123",
+                gender = "123",
+                age = 11,
+                hobby = "123"
+            };
+
+            userRepo.Add(user);
+
+
+            Uow.Save();
+        }
 
     }
 }
